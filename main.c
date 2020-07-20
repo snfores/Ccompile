c504121b6996082f3552fb06ef5b37f0a76a1b4b
shinji <sin1116twin@gmail.com>
2020-07-20 19:32:27 +0900

    分割コンパイル

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "9cc.h"
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    error_at(token->str, "引数の個数が正しくありません");
+    return 1;
+  }
+
+  // トークナイズする
+  user_input = argv[1];
+  token = tokenize();
+  Node *node = expr();
+
+  codegen(node);
+
+  return 0;
+}
